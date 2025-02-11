9bfa16330de114a3119aaa741ba1e4abd79ddc6e
Mustafa Olmezses <mustafaolmezses@gmail.com>
2025-02-11 20:57:35 +0300

    Initial Commit

---
+++ IntermediateSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IntermediateSwiftUI
+//
+//  Created by Mustafa Ölmezses on 11.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IntermediateSwiftUI/IntermediateSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntermediateSwiftUIApp.swift
+//  IntermediateSwiftUI
+//
+//  Created by Mustafa Ölmezses on 11.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct IntermediateSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
