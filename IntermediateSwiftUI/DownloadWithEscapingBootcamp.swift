7c9e03a37e0acba6b11dafeeddd1dfe06c39b939
Mustafa Olmezses <mustafaolmezses@gmail.com>
2025-02-22 22:17:28 +0300

    Download data with Escaping -> JSON CODABLE ESCAPİNG WEAK SELF

---
+++ IntermediateSwiftUI/DownloadWithEscapingBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  DownloadWithEscapingBootcamp.swift
+//  IntermediateSwiftUI
+//
+//  Created by Mustafa Ölmezses on 22.02.2025.
+//
+
+import SwiftUI
+
+struct DownloadWithEscapingBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DownloadWithEscapingBootcamp()
+}
